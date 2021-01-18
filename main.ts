08a9312f3e007f8bd17be783c2753b8fe8522028
dswembou <56019569+dswembou@users.noreply.github.com>
2021-01-18 20:37:56 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let projectile: Sprite = null
 scene.setBackgroundColor(1)
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -18,11 +17,26 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+let projectile = sprites.createProjectileFromSide(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, 50, 50)
 game.onUpdate(function () {
     controller.moveSprite(mySprite)
-    if (true) {
-    	
-    }
 })
 game.onUpdateInterval(5000, function () {
     projectile = sprites.createProjectileFromSide(img`
